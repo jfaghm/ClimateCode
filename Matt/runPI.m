19d44e4179b93b34c4d4fd4b6a2ed2efa265b636
Matthew Le <lematt@cs.umn.edu>
2012-05-30 16:58:15 -0500

    adding runPI.m

---
+++ Matt/runPI.m
@@ -0,0 +1,50 @@
+pressureDataPath = '/project/expeditions/lem/data/pressureLevelData_1979-present.nc';
+sstDataPath = '/project/expeditions/lem/data/sst_slp_eraInterim_1979-2010.nc';
+
+%Read in the data
+if ~exist('dataLoaded', 'var') || dataLoaded == false
+   lat = ncread(pressureDataPath, 'lat');
+   lon = ncread(pressureDataPath, 'lon');
+   time = ncread(pressureDataPath, 'time');
+   lev = ncread(pressureDataPath, 'lev') * .01;
+   [levels, sortedIndices] = sort(lev, 'descend');
+   temps = ncread(pressureDataPath, 'var130') - 273.15;
+   temps = temps(:, :, sortedIndices, :);
+   specificHumidity = ncread(pressureDataPath, 'var133');
+   specificHumidity = specificHumidity(:, :, sortedIndices, :);
+   mixingRatio = specificHumidity ./ (1 - specificHumidity);
+   sst = ncread(sstDataPath, 'var34') - 273.15;
+   centralPressure = ncread(sstDataPath, 'var134') * .01;
+   dataLoaded = true;
+end
+
+%Open up the matlabpool so that the parfor runs in parallel
+if matlabpool('size') == 0
+    matlabpool open
+end
+
+PIData = cell(size(time, 1), 1);
+
+for i = 1:size(time, 1)
+   PIData{i} = runPIHelper(sst(:, :, i), centralPressure(:,:,i), levels, temps(:,:,1:27,i), mixingRatio(:,:,1:27,i)); 
+end
+
+
+for currTime = 1:size(time, 1)
+    parfor i = 1:size(sst, 1)
+        vMaxRow = zeros(1, size(sst, 2));
+        for j = 1:size(sst, 2)
+            [~,vMax, ~, ifl] = mpikerry(sst(i, j, currTime), pres(i, j, currTime), levels, temps(i, j, :, currTime), mixRatio(i, j, :), currTime);
+            if ifl == 2
+                error('mpikerry failed');
+            end
+            vMaxRow(j) = vMax;
+        end
+        vMaxMat(i, :) = vMaxRow;
+        i
+    end
+PIData{currTime} = vMaxMat';
+end
+
+
+
