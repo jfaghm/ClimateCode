6a5d667e4e0e39e173f9ccd62f5df724bbd9466e
Matthew Le <lematt@cs.umn.edu>
2012-08-08 12:01:09 -0500

    removing SST Draft

--- Matt/ENSOStats.m
+++ Matt/ENSOStats.m
@@ -1,64 +1,43 @@
+function [averageElNino, averageNeutral, averageLaNina] =...
+    ENSOStats(hurCategory)
+%This function is used to calculate the average number of hurricanes that
+%occured in El Nino years, Neutral years, and La Nina years.
+%   Input: the smallest category hurricane that should be included in the
+%   calculation, 0 = tropical cyclone, and 1-5 corresponds to hurricane
+%   categories.  See condensedHurDat.mat documentation for other
+%   categories.
+%   Output: Average number of hurricanes that occured in each phase of ENSO
 
-if ~exist('dataLoaded', 'var') || dataLoaded == false
-    load condensedHurDat.mat;
-    load ENSOType.mat;
-end
-hurricanes = condensedHurDat(condensedHurDat(:, 1) >= 1950, :);
-%only use tropical storms and greater
-hurricanes = hurricanes(hurricanes(:, 10) >= 0, :);
-%exclude tropical storms
-if exist('OnlyHurricanes', 'var') && OnlyHurricanes == true
-    hurricanes = hurricanes(hurricanes(:, 10) >= 1, :);
-end
-%only get the hurricanes that are classified 3 - 5
-if exist('MajorHurricanesOnly', 'var') && MajorHurricanesOnly == true 
-    hurricanes = hurricanes(hurricanes(:, 10) >= 3, :);
-end  
+load /project/expeditions/lem/ClimateCode/Matt/matFiles/condensedHurDat.mat;
+load /project/expeditions/lem/ClimateCode/Matt/matFiles/ENSOType.mat
 
-numYearsElNino = sum(ENSOType(:) == 1);
-numYearsNeutral = sum(ENSOType(:) == 0);
-numYearsLaNina = sum(ENSOType(:) == -1);
+hurricanes = condensedHurDat(condensedHurDat(:, 1) >= 1950 & ...
+    condensedHurDat(:, 10) >= hurCategory, :);
 
-baseYear = 1950; %base year for ENSO type data
+baseYear = 1950;
 eIndex = 1; nIndex = 1; lIndex = 1;
-for i = 1:size(ENSOType, 1)
-    yearOfHurricane = baseYear + i - 1;
-    currentHurricanes = hurricanes(hurricanes(:, 1) == yearOfHurricane, :);
-    if exist('OnlyHurricanes', 'var') && OnlyHurricanes == true
-        currentHurricanes = currentHurricanes(currentHurricanes(:, 10) >= 1, :);
-    end
-    if exist('MajorHurricanesOnly', 'var') && MajorHurricanesOnly == true
-        currentHurricanes = currentHurricanes(currentHurricanes(:, 10) >= 3, :);
-    end
+
+ElNinoHurricanes = zeros(numelements(find(ENSOType == 1)), 1);
+NeutralHurricanes = zeros(numelements(find(ENSOType == 0)), 1);
+LaNinaHurricanes = zeros(numelements(find(ENSOType == -1)), 1);
+
+for i = 1:length(ENSOType)
+    currentYear = baseYear + i - 1;
+    currentHurricanes = hurricanes(hurricanes(:, 1) == currentYear, :);
     switch ENSOType(i)
         case 1
             ElNinoHurricanes(eIndex) = numelements(currentHurricanes);
-            eIndex = eIndex+1;
+            eIndex = eIndex + 1;
         case 0
             NeutralHurricanes(nIndex) = numelements(currentHurricanes);
-            nIndex = nIndex+1;
+            nIndex = nIndex + 1;
         case -1
             LaNinaHurricanes(lIndex) = numelements(currentHurricanes);
             lIndex = lIndex+1;
     end
 end
 
-averageElNino = sum(ElNinoHurricanes)/size(ElNinoHurricanes, 2);
-averageNeutral = sum(NeutralHurricanes)/ size(NeutralHurricanes, 2);
-averageLaNina = sum(LaNinaHurricanes)/ size(LaNinaHurricanes, 2);
-y = [averageElNino, averageNeutral, averageLaNina];
-e = [std(ElNinoHurricanes), std(NeutralHurricanes), std(LaNinaHurricanes)];
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+averageElNino = sum(ElNinoHurricanes)/length(ElNinoHurricanes);
+averageNeutral = sum(NeutralHurricanes)/ length(NeutralHurricanes);
+averageLaNina = sum(LaNinaHurricanes)/ length(LaNinaHurricanes);
+end
