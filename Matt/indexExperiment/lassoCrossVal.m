9d80f7770912643621a0530fb6b5f1c65426c8c0
Matthew Le <lematt@cs.umn.edu>
2012-10-11 16:01:02 -0500

    adding files

--- Matt/indexExperiment/lassoCrossVal.m
+++ Matt/indexExperiment/lassoCrossVal.m
@@ -1,9 +1,10 @@
-function [ypred, model, cc] = lassoCrossVal(predictors, target, k)
+function [ypred, model, cc, mse] = lassoCrossVal(predictors, target, k)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
 iteration = 1;
 model = zeros(size(predictors, 2), floor(length(target)/k));
+mse = zeros(floor(length(target)/k), 1);
 for i = 1:k:length(target)
    if i+k-1 > length(target)
        break
@@ -11,10 +12,13 @@ for i = 1:k:length(target)
    test = false(length(target) - mod(length(target), k), 1);
    test(i:i+k-1) = 1;
    train = ~test;
-   [B, fitInfo] = lasso(predictors(train, :), target(train));
+   [B, fitInfo] = lasso(predictors(train, :), target(train), 'NumLambda', 51);
    ypred(i:i+k-1, 1) = predictors(test, :) * B(:, find(min(fitInfo.MSE)));
    actuals(i:i+k-1, 1) = target(i:i+k-1);
    model(:, iteration) = B(:, find(min(fitInfo.MSE)));
+   
+   mse(iteration, :) = fitInfo.MSE(floor(length(fitInfo.MSE)/2));
+   
    iteration = iteration+1;
 end
 cc = corr(ypred, actuals);
