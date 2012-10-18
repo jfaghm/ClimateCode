73b9134d9cf9ad07146d0e5672659c3eb50b6799
Matthew Le <lematt@cs.umn.edu>
2012-10-18 15:16:02 -0500

    adding pdfs

--- Matt/scripts/EOFCrossValidationScript.m
+++ Matt/scripts/EOFCrossValidationScript.m
@@ -2,7 +2,27 @@ dir = '/project/expeditions/lem/ClimateCode/Matt/indexExperiment/results/paperDr
 addpath('/project/expeditions/lem/ClimateCode/Matt/indexExperiment');
 prefix = {'augOct', 'marOct'};
 postfix = {'AtlanticBasin', 'PacificBasin', 'JointBasins'};
+load /project/expeditions/ClimateCodeMatFiles/asoHurricaneStats.mat;
+leaveK = [1, 2, 4, 8];
 
+for i = 1:5
+    for j = 1:length(leaveK)
+        for k = 1:length(postfix)
+            t = load([dir 'augOct' postfix{k} 'EOFPCs.mat']);
+            [ypred, model, cc] = lassoCrossVal(t.PCs(:, 1:i), aso_tcs, leaveK(j), 0);
+            save([dir 'crossValidationModels/' 'augOct' postfix{k} 'Leave' ...
+                num2str(leaveK(j)) 'OutCrossValidationFirst' num2str(i) ...
+                'Components.mat'], 'ypred', 'model', 'cc');
+            ccMat(i, j, k) = cc;
+        end
+    end
+end
+
+
+
+
+
+%{
 k = 4;
 numPCs = 4;
 load /project/expeditions/lem/ClimateCode/Matt/matFiles/asoHurricaneStats.mat
