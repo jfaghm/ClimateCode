1edb7ac77f772e1378f9902804099ba34d63516c
Matthew Le <lematt@cs.umn.edu>
2012-10-31 14:57:24 -0500

    updating

--- Matt/indexExperiment/lassoCrossVal.m
+++ Matt/indexExperiment/lassoCrossVal.m
@@ -1,4 +1,4 @@
-function [ypred, model, cc, mse] = lassoCrossVal(predictors, target, k, useVars)
+function [ypred, model, cc, mse, Bmat] = lassoCrossVal(predictors, target, k)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 iteration = 1;
@@ -12,7 +12,7 @@ for i = 1:k:length(target)
    test(i:i+k-1) = 1;
    train = ~test;
    options = statset('UseParallel', 'always');
-   [B, fitInfo] = lasso(predictors(train, :), target(train), 'DFMax', useVars, 'Options', options);
+   [B, fitInfo] = lasso(predictors(train, :), target(train), 'Options', options);
    ypred(i:i+k-1, 1) = predictors(test, :) * B(:, fitInfo.MSE == min(fitInfo.MSE));
    actuals(i:i+k-1, 1) = target(i:i+k-1);
    model(:, iteration) = B(:, fitInfo.MSE == min(fitInfo.MSE));
@@ -21,6 +21,9 @@ for i = 1:k:length(target)
    y = actuals(i:i+k-1, 1);
    yHat = ypred(i:i+k-1, 1);
    mse(iteration, :) = mean((y - yHat).^2);
+   
+   Bmat(:, :, iteration) = B;
+   
    iteration = iteration+1;
 end
 cc = corr(ypred, actuals);
