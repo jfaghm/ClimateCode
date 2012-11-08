5d1c37d3f007edfdecd0342b658aff8aa08301a0
James Faghmous <jfaghm@gmail.com>
2012-11-07 21:22:18 -0600

    fixed bug in crossvalidation

--- James/lassoCrossValidation.m
+++ James/lassoCrossValidation.m
@@ -1,6 +1,7 @@
 %lassoCrossValidation.m
 %Runs a leave-k-out cross validation
 %NOTE: DOES NOT WORK IF X/k isn't a round number
+%to run [B, F, y_pred, mse] = lassoCrossValidation(X,Y, 8, 0.1) where X is NxM matrix and Y is 1xN vector
 
 function [B, F, y_pred fold_mse] =  lassoCrossValidation(x,y,k, lambda)
 assert(mod(length(x),k)==0,'X/k must be 0 i.e. 8 elements and leave 2 out.');
@@ -13,8 +14,8 @@ for i=1:k:size(x,1)
     test_y = y(i:i+k-1);
     train_y = y; 
     train_y(i:i+k-1)=[];
-    [B{i},F{i}] = lasso(train,train_y,'lambda',lambda);
-    y_pred{count} =test * B{i};
+    [B{count},F{count}] = lasso(train,train_y,'lambda',lambda);
+    y_pred{count} =test * B{count};
     fold_mse(count) = sum((test_y - y_pred{count}).^2);
     count = count+1;
 end
