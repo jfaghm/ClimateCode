aa2d98f34ef3b0758cfdd7f8a707b4f94fe072dd
Matthew Le <lematt@cs.umn.edu>
2012-11-09 13:46:54 -0600

    updating

--- James/lassoCrossValidation.m
+++ James/lassoCrossValidation.m
@@ -3,7 +3,12 @@
 %NOTE: DOES NOT WORK IF X/k isn't a round number
 %to run [B, F, y_pred, mse] = lassoCrossValidation(X,Y, 8, 0.1) where X is NxM matrix and Y is 1xN vector
 
-function [B, F, y_pred fold_mse] =  lassoCrossValidation(x,y,k, lambda)
+function [B, F, y_pred fold_mse] =  lassoCrossValidation(x,y,k, lambda, intercept)
+
+if nargin < 5
+    intercept = false;
+end
+
 assert(mod(length(x),k)==0,'X/k must be 0 i.e. 8 elements and leave 2 out.');
 assert(isscalar(lambda), 'lambda must be a scalar');
 count = 1;
@@ -16,6 +21,11 @@ for i=1:k:size(x,1)
     train_y(i:i+k-1)=[];
     [B{count},F{count}] = lasso(train,train_y,'lambda',lambda);
     y_pred{count} =test * B{count};
+    
+    if intercept == true
+        y_pred{count} = y_pred{count} + F{count}.Intercept;
+    end
+    
     fold_mse(count) = sum((test_y - y_pred{count}).^2);
     count = count+1;
 end
--- James/randomize_regression.m
+++ James/randomize_regression.m
@@ -1,46 +1,91 @@
 %randomize the hurricane counts and check robustness of model
 clear 
 close all
+
+if matlabpool('size') == 0
+    matlabpool open
+end
+
 load('regression_model_sst_data','pacific_indices_mar_oct');
 load('regression_model_sst_data','pac_pcs_aug_oct');
 load asoHurricaneStats
 predictors = [pacific_indices_mar_oct];
-num_iter = 100;
-lambda = 0.4;
-leave_k_out = 4;
-[W,~] = lasso(predictors,aso_tcs,'Lambda',lambda);
-real_cc = corr(predictors * W,aso_tcs);
+num_iter = 1000;
+lambda = 0;
+leave_k_out = 8;
+[W,f] = lasso(predictors,aso_tcs,'Lambda',lambda);
+real_cc = corr(predictors * W + f.Intercept,aso_tcs);
 
-[~, ~, pred_y, mm] = lassoCrossValidation(predictors, aso_tcs ,leave_k_out,lambda);
-real_xv_cc = corr(pred_y,aso_tcs);
+
+%% 
+pred = linearFitCrossVal(predictors, aso_tcs, leave_k_out);
+real_xv_cc_linear_fit = corr(pred,aso_tcs);
 
 parfor i=1:num_iter
     rand_ind = randperm(32);
     rand_tcs(:,i) = aso_tcs(rand_ind);
-    [B{i}, FitInfo{i}] = lasso(predictors, rand_tcs(:,i),'Lambda',lambda);
-    yhat{i} = predictors * B{i};
-    no_x_val_corr(i) = corr(yhat{i},rand_tcs(:,i));  
-    [BB,FF,x_val_y_hat, mse{i}] = lassoCrossValidation(predictors, rand_tcs(:,i) ,leave_k_out,lambda);
-    cc(i) = corr(x_val_y_hat,rand_tcs(:,i));
-   
+    
+    x_val_y_hat_linear_fit = linearFitCrossVal(predictors, rand_tcs(:, i), leave_k_out);
+    ccLinearfit(i) = corr(x_val_y_hat_linear_fit,rand_tcs(:,i));
 end
 
+%% 
+
+mdl = LinearModel.fit(predictors, aso_tcs);
+ypred = predict(mdl, predictors);
+real_cc_linear_fit = corr(ypred, aso_tcs);
+
+parfor i = 1:num_iter
+   mdl = LinearModel.fit(predictors, rand_tcs(:, i));
+   ypred = predict(mdl, predictors);
+   ccLinearFitNoXVal(i) = corr(ypred, rand_tcs(:, i));
+end
+
+%% 
+[~, ~, pred_y, mm] = lassoCrossValidation(predictors, aso_tcs ,leave_k_out,lambda, true);
+real_xv_cc = corr(pred_y,aso_tcs);
+parfor i=1:num_iter
+    %rand_ind = randperm(32);
+    %rand_tcs(:,i) = aso_tcs(rand_ind);
+    [B{i}, FitInfo{i}] = lasso(predictors, rand_tcs(:,i),'Lambda',lambda);
+    yhat{i} = predictors * B{i} + FitInfo{i}.Intercept;
+    no_x_val_corr(i) = corr(yhat{i},rand_tcs(:,i));  
+    [BB{i},FF,x_val_y_hat{i}, mse{i}] = lassoCrossValidation(predictors, rand_tcs(:,i) ,leave_k_out,lambda, true);
+    cc(i) = corr(x_val_y_hat{i},rand_tcs(:,i));
+end
+
+%% 
+
+numCols = 2;
+
 figure
-subplot(2,1,1)
+subplot(2,numCols,1)
 hist(no_x_val_corr);
 hold on
 vline(real_cc);
-title(['Leave ' num2str(leave_k_out) ' out Null distribution for no-cross validation randomization - lambda = ' num2str(lambda) ' N = ' num2str(num_iter) ' random runs']);
+title(['No-cross validation randomization - lambda = ' num2str(lambda) ' N = ' num2str(num_iter) ' random runs']);
 hold off
 
-subplot(2,1,2)
+subplot(2,numCols,2)
 hist(cc)
 hold on
 vline(real_xv_cc);
-title(['Leave ' num2str(leave_k_out) ' out Null distribution for cross validation randomization - lambda = ' num2str(lambda) ' N = ' num2str(num_iter) ' random runs']);
+title(['Leave ' num2str(leave_k_out) ' out cross validation randomization - lambda = ' num2str(lambda) ' N = ' num2str(num_iter) ' random runs']);
 hold off
 
 
+subplot(2, 2, 3)
+hist(ccLinearFitNoXVal)
+hold on
+vline(real_cc_linear_fit);
+title(['No-cross validation randomization - linear fit model']);
+hold off
+
+subplot(2, 2, 4);
+hist(ccLinearfit);
+hold on
+vline(real_xv_cc_linear_fit);
+title(['Leave ' num2str(leave_k_out) ' out Cross validation randomization - linear fit model']);
 
 
 
